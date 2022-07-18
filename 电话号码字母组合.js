c1376c0169c7ad1a9876a6faa97c46ee53a5e0ff
wudongyu <wudongyu@bilibili.com>
2022-07-18 19:32:06 +0800

    7.18

---
+++ 电话号码字母组合.js
@@ -0,0 +1,43 @@
+/**
+    给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+    给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+    const map = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz",
+    };
+ */
+
+var letterCombinations = (digits) => {
+    const map = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz",
+    };
+    const res = []
+    const recursiveCall = (newDigit, i) => {
+        if (i > digits.length - 1) {
+            res.push(newDigit)
+            return
+        }
+        const letters = map[digits[i]]
+        for (const l of letters) {
+            recursiveCall( newDigit+l , i+1)
+        }
+    }
+}
+
+
+
