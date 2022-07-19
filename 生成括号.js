52e90485a3e4d85b97648cd3a694fc7fe09b72ea
bigbald0726 <2427094899@qq.com>
2022-07-20 04:01:29 +0800

    7.19

--- 生成括号.js
+++ 生成括号.js
@@ -10,17 +10,25 @@
 
 
 
-const generateParenthesis = (n, result = []) => {
+const generateParenthesis = (n) => {
 
     // left 为 左括号个数，right 为 右括号个数 个数等于0，则push。
     // 条件就是，左括号小于n时，右括号个数小于左括号时，进入下一个增加括号的循环
-    const backtrack = (left, right, str) => {
-        if (left === n && right === n) return result.push(str)
-        if (left < n) backtrack(left + 1, right, str + "(")
-        if (right < left) backtrack(left, right + 1, str + ")")
+    let res = []
+    const recursive = (left, right, temp) => {
+        if (left<n) {
+            recursive(left+1,right,temp+'(')
+        }
+        if (right<left) {
+            recursive(left,right+1,temp+')')
+        }
+        if(right===n && left===n) {
+            res.push(temp)
+            return
+        }
     }
-    backtrack(0, 0, "")
-    return result
+    recursive(0,0,'')
+    return res
 }
 
 var test = 2
